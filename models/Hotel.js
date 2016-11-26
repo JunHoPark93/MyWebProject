c08006191bc8f083cdea6dd84dca334b4f6aff27
JunHo <pjh374968@gmail.com>
2016-11-26 20:50:36 +0900

    register hotel and save in db

---
+++ models/Hotel.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = mongoose.Schema({
+	    houseName: {type: String, trim: true, 'default':''},
+	    region: {type: String, trim:true, 'default':''},					
+	    writer: {type: String},
+	    introduction: {type: String, trim: true},
+      select: {type: String, 'default':''},
+      price: {type: String, 'default':''},
+	    created_at: {type: Date, index: {unique: false}, 'default': Date.now},
+	    updated_at: {type: Date, index: {unique: false}, 'default': Date.now}
+	});
+
+  var Hotel = mongoose.model('Hotel', schema);
+
+  module.exports = Hotel;
---
+++ routes/hotelregister.js
@@ -0,0 +1,52 @@
+var express = require('express'),
+    User = require('../models/User'),
+    Hotel = require('../models/Hotel');
+var router = express.Router();
+
+
+function needAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    console.log("auth");
+    next();
+  } else {
+    res.status(401).json({message: 'Not authorized'});
+  }
+}
+
+router.post('/', function(req, res, next) {
+  console.log(req.body.houseName);
+  var ses = req.session;
+  console.log(ses);
+  console.log("---------");
+  console.log(ses.passport.user);
+
+  var newHotel = new Hotel({
+    houseName : req.body.houseName,
+    region : req.body.region,
+    writer : ses.passport.user,
+    introduction : req.body.introduction,
+    select : req.body.select,
+    price : req.body.price
+  });
+
+
+  newHotel.save(function(err) {
+    if(err) {
+      return next(err);
+    } else {
+      res.redirect('/hotelregister');
+    }
+  });
+});
+
+// router.get('/:id/hotelregister', function(req, res, next) {
+//
+//   // 넘겨받은 id로 조회한다
+//   User.findById(req.params.id, function(err, post) {
+//     if (err) {
+//       return next(err);
+//     }
+//     res.render('hotelregister', {post: post}); // 조회된 post를 edit.jade로 넘겨준다
+//   });
+// });
+module.exports = router;
