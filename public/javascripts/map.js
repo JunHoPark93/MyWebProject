5c37002013a4f690ff9125eae6c1bc2f0b29eead
JunHo <pjh374968@gmail.com>
2016-12-11 15:21:04 +0900

    add marker of hotel in Daum map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -5,8 +5,44 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
 };
 
 var map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴
+var roadNum = $('#roadNum').text();
+
+console.log(roadNum);
 
 map.relayout();
+
+var geocoder = new daum.maps.services.Geocoder();
+
+geocoder.addr2coord(roadNum, function(status, result) {
+
+    // 정상적으로 검색이 완료됐으면
+     if (status === daum.maps.services.Status.OK) {
+
+        var coords = new daum.maps.LatLng(result.addr[0].lat, result.addr[0].lng);
+
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        var marker = new daum.maps.Marker({
+            map: map,
+            position: coords
+        });
+
+        // 인포윈도우로 장소에 대한 설명을 표시합니다
+        var infowindow = new daum.maps.InfoWindow({
+            content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
+        });
+        infowindow.open(map, marker);
+
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+    }
+});
+
+// //마커를 미리 생성
+// var marker = new daum.maps.Marker({
+// 	 position: new daum.maps.LatLng(37.537187, 127.005476),
+// 	 map: map
+// });
+
 
 // //본 예제에서는 도로명 주소 표기 방식에 대한 법령에 따라, 내려오는 데이터를 조합하여 올바른 주소를 구성하는 방법을 설명합니다.
 // $('#find_address,#host_postcode,#sample4_address').click(function() {
